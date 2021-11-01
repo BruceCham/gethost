1a66a78ad58a18ba7601f34e6eb0967b4a01b21b
zhanglitao1 <zhanglitao1@tal.com>
2021-11-01 18:32:55 +0800

    feat: 修改github的查询地址

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 let axios = require('axios');
 
 const getUrlIp = async () => {
-    let res = await axios.get('https://github.com.ipaddress.com/')
+    let res = await axios.get('https://websites.ipaddress.com/github.com')
     let val = res.data.match(/(?<=\.com\/ipv4\/)[\d\.]+/g)
     return [val[0], 'github.com']
 }
-
 // 2. 获取github域名查询
 const getDomainIp = async () => {
-    let res = await axios.get('https://fastly.net.ipaddress.com/github.global.ssl.fastly.net')
+    let res = await axios.get('https://websites.ipaddress.com/github.global.ssl.fastly.net')
     let val = res.data.match(/(?<=\.com\/ipv4\/)[\d\.]+/g)
     return [val[0], 'github.global.ssl.fastly.net']
 }
 // 3. 获取github静态资源ip
 const getStaticIp = async () => {
-    let res = await axios.get('https://github.com.ipaddress.com/assets-cdn.github.com')
+    let res = await axios.get('https://websites.ipaddress.com/assets-cdn.github.com')
     let val = res.data.match(/(?<=\.com\/ipv4\/)[\d\.]+/g)
     return [...new Set(val)].map((item) => {
         return [item, 'assets-cdn.github.com']
     })
 }
-
-// 4. 生成内容
-Promise.all([getUrlIp(), getDomainIp(), getStaticIp()]).then((res) => {
-    let val = [res[0], res[1], ...res[2]]
-    let str = copyHost(val)
-    console.log(str)
-})
-
-// 5. 拼接
+// 4. 拼接
 const copyHost = (val) => {
     let str = ''
     val.forEach((item) => {
@@ -36,3 +27,9 @@ const copyHost = (val) => {
     })
     return str
 }
+// 5. 生成内容
+Promise.all([getUrlIp(), getDomainIp(), getStaticIp()]).then((res) => {
+    let val = [res[0], res[1], ...res[2]]
+    let str = copyHost(val)
+    console.log(str)
+})
